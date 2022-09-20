11334df960259f842bcfdcbf839a25a2775cb442
jeffrey <Wo1025792161>
2022-09-20 21:39:04 +0800

    更新显示

--- static/js/Tools/NewDate.js
+++ static/js/Tools/NewDate.js
@@ -13,10 +13,7 @@ class NewDate{
      */
     static GetDate(time = +new Date(), OnlyDate=true) {
         let date = new Date(time + 8 * 3600 * 1000);
-        let ms = date.getMilliseconds();
 
-        date = date.toJSON();
-        date = date + " : " + ms;
         if(OnlyDate){
             date = date.split('T')[0];
         }
