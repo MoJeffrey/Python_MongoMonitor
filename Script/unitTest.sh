449f4729086fc1074e9d5b7ac57119ec250ef4b3
jeffrey <Wo1025792161>
2022-10-16 13:15:49 +0800

    (BBE-???) Edit CICD

--- Script/unitTest.sh
+++ Script/unitTest.sh
@@ -6,5 +6,5 @@ export CONFIG_PATH=./config/Config.ini
 cd ./src
 pip install -r requirement.txt
 pytest ../UnitTest/Test_Main.py --junitxml=Report.xml
-python ../UnitTest/Test_Main.py
+#python ../UnitTest/Test_Main.py
 echo "Unit test done!"
